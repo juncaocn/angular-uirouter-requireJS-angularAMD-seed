4bfd2169c4062ff13ceb25b7139f62db97900086
jun cao <63168584@qq.com>
2017-03-29 17:05:14 +0800

    comment

--- js/Ctrl_A.js
+++ js/Ctrl_A.js
@@ -1,5 +1,7 @@
-define(['app'],function(app){
-    app.controller('ACtrl',['$scope',function(_scope){
-        _scope.name='xxx';
+define(['app'], function (app) {
+    'use strict';
+
+    app.controller('ACtrl', ['$scope', function (_scope) {
+        _scope.name = 'xxx';
     }]);
 });
--- js/Ctrl_B.js
+++ js/Ctrl_B.js
@@ -1,9 +1,17 @@
-define(['app'],function(app){
-    app.controller('BCtrl',['$scope',function(_scope){
-        _scope.name='yyy';
+define(['app'], function (app) {
+    'use strict';
+
+    /**
+     * 
+     */
+    app.controller('BCtrl', ['$scope', function (_scope) {
+        _scope.name = 'yyy';
     }]);
 
-    app.controller('CCtrl',['$scope',function(_scope){
-        _scope.name='ZZZ';
+    /**
+     * 
+     */
+    app.controller('CCtrl', ['$scope', function (_scope) {
+        _scope.name = 'ZZZ';
     }]);
 });
