a2976e597f0c3723a2290bc98eeb171da0e6fd56
jun cao <63168584@qq.com>
2017-03-30 15:36:44 +0800

    add commonjs style

--- js/Ctrl_A.js
+++ js/Ctrl_A.js
@@ -2,12 +2,21 @@
  * angular app controllers define
  * @auther caojun@qingdao
  */
-define(['app'], function (app) {
+
+/*define(['app'], function (app) {
     'use strict';
 
-    /**
-     * 
-     */
+    
+    app.controller('ACtrl', ['$scope', function (_scope) {
+        _scope.name = 'xxx';
+    }]);
+});*/
+
+/**
+ * 兼容commonjs写法
+ */
+define(function (require, exports, module) {
+    var app = require('app');
     app.controller('ACtrl', ['$scope', function (_scope) {
         _scope.name = 'xxx';
     }]);
--- js/Ctrl_B.js
+++ js/Ctrl_B.js
@@ -1,14 +1,15 @@
 /**
- * angular app controllers define
+ * angular app controllers define (multi controller)
  * @auther caojun@qingdao
  */
 define(['app'], function (app) {
     'use strict';
 
     /**
-     * 
+     * Serv_a在更早的Ctrl_Menu中已经require，此处可直接引用
      */
-    app.controller('BCtrl', ['$scope', function (_scope) {
+    app.controller('BCtrl', ['$scope','Serv_a', function (_scope,Serv_a) {
+        Serv_a.serv1();
         _scope.name = 'yyy';
     }]);
 
