6a16413cd357a833fb977d0b4e3b8377f895df60
jun cao <63168584@qq.com>
2017-03-31 08:44:32 +0800

    default

--- js/Ctrl_A.js
+++ js/Ctrl_A.js
@@ -2,11 +2,9 @@
  * angular app controllers define
  * @auther caojun@qingdao
  */
-
 define(['app'], function (app) {
     'use strict';
 
-    
     app.controller('ACtrl', ['$scope', function (_scope) {
         _scope.name = 'xxx';
     }]);
