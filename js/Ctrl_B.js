3b22fb92e20d50533973276c1a1cf44d3b66353a
jun cao <63168584@qq.com>
2017-03-31 08:37:41 +0800

    commonjs

--- js/Ctrl_B.js
+++ js/Ctrl_B.js
@@ -2,9 +2,32 @@
  * angular app controllers define (multi controller)
  * @auther caojun@qingdao
  */
-define(['app'], function (app) {
+// define(['app'], function (app) {
+//     'use strict';
+
+//     /**
+//      * Serv_a在更早的Ctrl_Menu中已经require，此处可直接引用
+//      */
+//     app.controller('BCtrl', ['$scope','Serv_a', function (_scope,Serv_a) {
+//         Serv_a.serv1();
+//         _scope.name = 'yyy';
+//     }]);
+
+//     /**
+//      * 
+//      */
+//     app.controller('CCtrl', ['$scope', function (_scope) {
+//         _scope.name = 'ZZZ';
+//     }]);
+// });
+
+/**
+ * commonjs style
+ */
+define(function(require,exports, module){
     'use strict';
 
+    var app = require('app');
     /**
      * Serv_a在更早的Ctrl_Menu中已经require，此处可直接引用
      */
