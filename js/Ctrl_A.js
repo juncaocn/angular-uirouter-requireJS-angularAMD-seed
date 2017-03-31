706e89198aecb786a2d194e19a667c4da4c1b66f
jun cao <63168584@qq.com>
2017-03-31 08:39:28 +0800

    default

--- js/Ctrl_A.js
+++ js/Ctrl_A.js
@@ -3,20 +3,10 @@
  * @auther caojun@qingdao
  */
 
-/*define(['app'], function (app) {
+define(['app'], function (app) {
     'use strict';
 
     
-    app.controller('ACtrl', ['$scope', function (_scope) {
-        _scope.name = 'xxx';
-    }]);
-});*/
-
-/**
- * 兼容commonjs写法
- */
-define(function (require, exports, module) {
-    var app = require('app');
     app.controller('ACtrl', ['$scope', function (_scope) {
         _scope.name = 'xxx';
     }]);
